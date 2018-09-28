7245e5d492f5c5de5f70a5a569357cace36da0bb
lipengfei <hudalpf@163.com>
2018-09-28 17:52:13 +0800

    fix bugs in add_users.sh

--- lab504/init_run/add_users.sh
+++ lab504/init_run/add_users.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 groupadd docker  #增加docker组
-for line in $(cat user_passwd)
+while read line
 do
-	user=$(echo $line | awk '{print $1}')
-	pwd=$(echo $line | awk '{print $2}')
-	# create users
-	useradd -d /home/$user -m $user
-	echo "${pwd}"| passwd --stdin $user 
-	sed -i "/^root/a\\${user} ALL = (ALL:ALL) NOPASSWD:ALL" /etc/sudoers
-	# add all users to docker group
-	usermod -aG docker $user
-done
+    user=`echo $line | awk '{print $1}'`
+    key=`echo $line | awk '{print $2}'`
+    # create users    
+    echo "create user [$user]"
+    useradd -d /home/$user -m $user
+    echo $key | passwd --stdin $user
+    sed -i "/^root/a\\${user} ALL = (ALL:ALL) NOPASSWD:ALL" /etc/sudoers
+    # add all users to docker group
+    usermod -aG docker $user
+done < user_passwd
