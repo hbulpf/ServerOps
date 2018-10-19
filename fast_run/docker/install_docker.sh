a42b1b16d4ab68fab3528f82f21fa8e5a30c5e1b
lipengfei <hudalpf@163.com>
2018-10-19 10:04:06 +0800

    fix bug @ install_docker.sh

--- fast_run/docker/install_docker.sh
+++ fast_run/docker/install_docker.sh
@@ -34,6 +34,9 @@ sudo usermod -aG docker $USER
 # curl -sSL http://oyh1cogl9.bkt.clouddn.com/setmirror.sh | sh -s reg-mirror.qiniu.com  #使用七牛云镜像加速
 #使用aliyun镜像加速
 if [[ ! -f /etc/docker/daemon.json ]]; then 
+    if [[ ! -d /etc/docker ]]; then 
+        mkdir -p /etc/docker
+    fi
 	touch /etc/docker/daemon.json
 fi
 cat <<EOF >>/etc/docker/daemon.json
