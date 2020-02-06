b67160c9a612a46c483b85f8a36b571102b291eb
lipengfei <hudalpf@163.com>
2020-02-06 12:09:42 +0800

    fastrun

--- fast_run/grant/add_a_user.sh
+++ fast_run/grant/add_a_user.sh
@@ -5,4 +5,4 @@ echo "create user [$user]"
 useradd -d /home/$user -m $user
 echo $key | passwd --stdin $user
 sed -i "/^root/a\\${user} ALL = (ALL:ALL) NOPASSWD:ALL" /etc/sudoers
-usermod -aG docker chenqiren
+usermod -aG docker ${user}
