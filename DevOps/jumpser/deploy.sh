1b76cdc98f6a720136556850b41b58c2c55f0071
lipengfei <hudalpf@163.com>
2020-09-01 02:17:31 +0800

    部署 jumpserver

--- DevOps/jumpser/deploy.sh
+++ DevOps/jumpser/deploy.sh
@@ -14,9 +14,9 @@ else
 fi
 
 
-bnHXlBjmjzVW5i64Y4v21TOzszSZHNEIbAynTh6cWJTnNQvqTV
+SECRET_KEY='bnHXlBjmjzVW5i64Y4v21TOzszSZHNEIbAynTh6cWJTnNQvqTV'
 
-V0y4ymfe9V1IVnYF
+BOOTSTRAP_TOKEN='V0y4ymfe9V1IVnYF'
 
 
 
@@ -28,3 +28,21 @@ docker run --name jms_all -d \
   -v /opt/jumpserver/mysql:/var/lib/mysql \
   --privileged=true \
   jumpserver/jms_all:v2.2.1
+
+
+docker run --name jms_all_rd -d \
+  -v /opt/jumpserver/data:/opt/jumpserver/data \
+  -p 81:80 \
+  -p 2222:2222 \
+  -e SECRET_KEY=$SECRET_KEY \
+  -e BOOTSTRAP_TOKEN=$BOOTSTRAP_TOKEN \
+  -e DB_HOST=139.9.179.15 \
+  -e DB_PORT=3306 \
+  -e DB_USER=jumpserver \
+  -e DB_PASSWORD=szSZHNEIbAynTh6c \
+  -e DB_NAME=jumpserver \
+  --privileged=true \
+  jumpserver/jms_all:v2.2.1
+
+
+  szSZHNEIbAynTh6c
